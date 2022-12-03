8a5fe3833879b601e25218daaa1672f42ed5f4cb
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2022-12-03 14:05:11 +0200

    Code clean up on user collection image controller and service.

--- src/main/java/gr/thegoodsideofe1/tourguide/controllers/UserCollectionImageController.java
+++ src/main/java/gr/thegoodsideofe1/tourguide/controllers/UserCollectionImageController.java
@@ -1,18 +1,9 @@
 package gr.thegoodsideofe1.tourguide.controllers;
 
-import gr.thegoodsideofe1.tourguide.aes.AES_ENCRYPTION;
-import gr.thegoodsideofe1.tourguide.entities.User;
-import gr.thegoodsideofe1.tourguide.entities.UserCollectionImage;
 import gr.thegoodsideofe1.tourguide.services.UserCollectionImageService;
-import gr.thegoodsideofe1.tourguide.services.UserCollectionService;
-import gr.thegoodsideofe1.tourguide.services.UserService;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,12 +13,6 @@ public class UserCollectionImageController {
 
     @Autowired
     UserCollectionImageService userCollectionImageService;
-    @Autowired
-    UserCollectionService userCollectionService;
-    @Autowired
-    UserService userService;
-    @Autowired
-    AES_ENCRYPTION aes_encryption;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public ResponseEntity<?> add(@RequestBody Map<String, String> requestBody) {
@@ -37,10 +22,5 @@ public class UserCollectionImageController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> remove(@RequestBody Map<String, String> requestBody, @PathVariable Integer id) {
         return userCollectionImageService.removeImageFromCollection(requestBody, id);
-    }
-
-    private String[] getUserDetailsFromJWT(String token) throws Exception {
-        String decryptedString = aes_encryption.decrypt(token);
-        return decryptedString.split(",");
     }
 }
